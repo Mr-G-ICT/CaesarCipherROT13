7249406e80b96061cf4331334b2c234d091a5f56
Mr B Gristwood <bgristwood@outlook.com>
2020-06-02 09:52:15 +0100

    built in the set method

---
+++ CaesarCipherROT13/CaesarCipher.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaesarCipherROT13
+{
+    class CaesarCipher
+    {
+        private string wordToEncrypt;
+
+        public string SetwordToEncrypt
+        {
+            //Set the string we are going to convert, do not use a get 
+            //method as we don't ever need to get the string
+            set { wordToEncrypt = value; }
+        }
+        public string choice { get; set; }
+       
+      public static string ROTCipher() 
+        {
+/*************************************************************************/
+/* Name: caesar cipher
+/* Description: a standard ROT 13 encrypter/decrypter.
+/* inputs: the word, wether you want to encrypt or decrypt
+/* outputs: the encrypted/decrypted string
+/* IMProvements: make it work for upper and lower case, will need more formatting tho'
+/************************************************************************/
+
+            const int SHIFT = 13;
+            string encryptedString = "";
+            int ASCIIletter;
+
+            for(int count = 0; count <= wordToEncrypt.Length - 1; count++)
+            {
+               
+                ASCIIletter = (int)Convert.ToChar(wordToEncrypt[count]);
+                
+                //check if it's a letter
+                if (ASCIIletter >= 65 && ASCIIletter <= 90)
+                {
+                    if (choice == "encrypt")
+                    {
+                        //this is for decrypt
+                        ASCIIletter = ASCIIletter + SHIFT;
+
+                        if (ASCIIletter >= 90)
+                        {
+                            int diff = ASCIIletter - 90;
+                            ASCIIletter = 64 + diff;
+                        }
+
+                    }
+                    else
+                    {
+                        //this is for decrypt
+                        ASCIIletter = ASCIIletter - SHIFT;
+                        if (ASCIIletter <= 65)
+                        {
+                            int diff = 65 - ASCIIletter;
+                            ASCIIletter = 91 - diff;
+                        }
+
+                    }
+
+                }
+                encryptedString = encryptedString + Convert.ToChar(ASCIIletter);
+            }
+
+            return encryptedString;
+        }
+
+    }
+    }
+}
--- CaesarCipherROT13/Program.cs
+++ CaesarCipherROT13/Program.cs
@@ -6,71 +6,19 @@ namespace CaesarCipherROT13
     {
         static void Main(string[] args)
         {
+            CaesarCipher MyROTCipher = new CaesarCipher;
             string word;
             string choice;
             Console.WriteLine("enter your string");
-            word = Console.ReadLine();
+            MyROTCipher.wordtoEncrypt = Console.ReadLine();
 
             Console.WriteLine("do you want to encrypt or decrypt");
-            choice = Console.ReadLine();
+            MyROTCipher.choice = Console.ReadLine();
 
-            word = ROTCipher(word, choice);
+            word = MyROTCipher.ROTCipher;
 
             Console.WriteLine(word);
         }
 
-        public static string ROTCipher(string wordToEncrypt, string choice) 
-        {
-/*************************************************************************/
-/* Name: caesar cipher
-/* Description: a standard ROT 13 encrypter/decrypter.
-/* inputs: the word, wether you want to encrypt or decrypt
-/* outputs: the encrypted/decrypted string
-/* IMProvements: make it work for upper and lower case, will need more formatting tho'
-/************************************************************************/
-
-            const int SHIFT = 13;
-            string encryptedString = "";
-            int ASCIIletter;
-
-            for(int count = 0; count <= wordToEncrypt.Length - 1; count++)
-            {
-               
-                ASCIIletter = (int)Convert.ToChar(wordToEncrypt[count]);
-                
-                //check if it's a letter
-                if (ASCIIletter >= 65 && ASCIIletter <= 90)
-                {
-                    if (choice == "encrypt")
-                    {
-                        //this is for decrypt
-                        ASCIIletter = ASCIIletter + SHIFT;
-
-                        if (ASCIIletter >= 90)
-                        {
-                            int diff = ASCIIletter - 90;
-                            ASCIIletter = 64 + diff;
-                        }
-
-                    }
-                    else
-                    {
-                        //this is for decrypt
-                        ASCIIletter = ASCIIletter - SHIFT;
-                        if (ASCIIletter <= 65)
-                        {
-                            int diff = 65 - ASCIIletter;
-                            ASCIIletter = 91 - diff;
-                        }
-
-                    }
-
-                }
-                encryptedString = encryptedString + Convert.ToChar(ASCIIletter);
-            }
-
-            return encryptedString;
-        }
-
-    }
+    
 }
