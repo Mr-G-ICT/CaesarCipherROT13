4bec2060cf98007a6be5e9643eeed632f0da4010
Mr B Gristwood <bgristwood@outlook.com>
2020-06-04 15:41:41 +0100

    Built in the start of a BFID cipher. Finished the Vernam cipher, still will nto take in numbers or letters, think this is due to the design of the cipher.

--- CaesarCipherROT13/CaesarCipher.cs
+++ CaesarCipherROT13/CaesarCipher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace CaesarCipherROT13
@@ -101,10 +102,10 @@ namespace CaesarCipherROT13
         {
             /*****************************************************************************/
             /* Name: Vernam Cipher
-            /* Description: standard vernam cipher that works with upper, lower and special characters
+            /* Description: standard vernam cipher that works with upper, lower
             /* Inputs: word to convert, wether it is encryption or decryption
             /* outputs: an encrypted word
-            /* Improvements: the encryption now works, now on to the decryption 
+            /* Improvements: needs to work for Symbols, but vernam doesn't do that.
             /*****************************************************************************/
             
             string encryptedString ="";
@@ -119,41 +120,18 @@ namespace CaesarCipherROT13
             {
                 UpperOrLower = 65;
                 ASCIILetter = (int)Convert.ToChar(wordToEncrypt[wordCount]);
-
                 //if it's a lowercase letter compensate
-                if(ASCIILetter > 90)
+                if(ASCIILetter >= 97 && ASCIILetter <= 122)
                 {
+                    Console.WriteLine("in here");
                     UpperOrLower = UpperOrLower + 32;
                 }
 
                     //work out the letter of the alphabet in standard form e.g. a=1, b=2
                     ASCIILetter = ASCIILetter - UpperOrLower;
 
-                Console.WriteLine(ASCIILetter);
-
-
-                //DOESN'T WORK...WILL Encrypt, but won't decrypt
-                const string VALIDSYMBOLS = @"\|!#$%&/()=?»«@£§€{}.-;'<>_,";
-                //this is the bit where i put in symbols, if the number is not between 1 and 26 
-                //then it has to be a symbol, so work this out.
-                if ((ASCIILetter < 1 || ASCIILetter > 26) && choice != "decrypt")
-                {
-                    //so if the symbol is in the list, convert and encrypt otherwise return error
-                    for (int symbolCount = 0; symbolCount <= VALIDSYMBOLS.Length - 1; symbolCount++)
-                    {
-                        if (wordToEncrypt[wordCount] == VALIDSYMBOLS[symbolCount])
-                        {
-                            ASCIILetter = symbolCount + 75;
-                            break;
-                        }
-                    }
-                }
-                else {
-                    ASCIILetter = ASCIILetter - 75;
-
-                
-                }
-
+                //when fixed put in here all my research says this won't work in this algorithm
+                //ASCIILetter = encryptDecryptSymbols(ASCIILetter);
 
                 //if we have reached the end of the key, reset it so that we can reuse the word
                 if (keyCount == MYVERNAMKEY.Length)
@@ -162,7 +140,7 @@ namespace CaesarCipherROT13
                 }
 
                     //reset this every time, as i need to check with each loop if the letter is upper or lower
-                    keyUpperOrLower = 65;
+                    keyUpperOrLower = 64;
                     keyLetter = (int)Convert.ToChar(MYVERNAMKEY[keyCount]);
 
                     //if it's a lowercase letter compensate - this is to save time when changing keys
@@ -180,7 +158,6 @@ namespace CaesarCipherROT13
                 {
 
                     ASCIILetter = ASCIILetter - keyLetter;
-                    Console.WriteLine("decrypt" + ASCIILetter);
                     if(ASCIILetter < 0)
                     {
                         ASCIILetter = ASCIILetter + 26;
@@ -191,7 +168,7 @@ namespace CaesarCipherROT13
                     ASCIILetter = ASCIILetter + keyLetter;
 
                     //if the conversion goes beyond 26 then go back to start of the alphabet
-                    if (ASCIILetter >= 26 && ASCIILetter <=75)
+                     if (ASCIILetter >= 26 && ASCIILetter <=75)
                     {
                         ASCIILetter = ASCIILetter - 26;
 
@@ -202,7 +179,6 @@ namespace CaesarCipherROT13
                     keyCount = keyCount + 1;
 
                 //add the encrypted letter to the end of the string.
-                Console.WriteLine("final letter"+ ASCIILetter + UpperOrLower);
                 encryptedString = encryptedString + Convert.ToChar( (ASCIILetter + UpperOrLower));
             }
 
@@ -210,6 +186,89 @@ namespace CaesarCipherROT13
             return encryptedString;
         }
 
+
+        public int encryptDecryptSymbols(int LetterToConvert)
+        {
+/***********************************************************************/
+/* CURRENTLY UNUSED - Was going to use this as part of vernam, but wouldnt work */
+/* Description: used to encrypt symbols from a list of allowed
+/* Inputs: a letter not recognised as an upper or lower case letter
+/* outputs: the position in the array of the symbol identified
+/*************************************************************************/
+
+            //DOESN'T WORK...WILL Encrypt, but won't decrypt
+            const string VALIDSYMBOLS = @"\|!#$%&/()=?»«@£§€{}.-;'<>_,";
+            //this is the bit where i put in symbols, if the number is not between 1 and 26 
+            //then it has to be a symbol, so work this out.
+            if ((LetterToConvert < 1 || LetterToConvert > 26) && choice != "decrypt")
+            {
+                //so if the symbol is in the list, convert and encrypt otherwise return error
+                for (int symbolCount = 0; symbolCount <= VALIDSYMBOLS.Length - 1; symbolCount++)
+                {
+                    if (Convert.ToChar((LetterToConvert + 65)) == VALIDSYMBOLS[symbolCount])
+                    {
+                        LetterToConvert = symbolCount - 26;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                LetterToConvert = LetterToConvert + 26;
+            }
+            return LetterToConvert;
+
+        }
+
+public string BFIDCipher()
+        {
+            /*********************************************************************/
+            /* Name: BFIDCipher
+            /* Description: Learning how to construct a BFID cipher, this is a 2d array to store an encryption key
+            /* then by multiplying the row and column can encrypt a letter       */
+            /* Inputs: Word to Encrypt, taken from get fetch at top, a choice to encrypt or decrypt
+            /* Outputs: an encrypted string 
+            /***********************************************************************/
+            string encryptedString = "";
+            //removed J as we only have a 25 square and 26 letters of the alphabet
+            //IMPROVEMENT extend to include more letters/symbols/numbers
+            char[,] EncryptionKey = { {'R', 'A', 'N', 'C', 'H' } ,
+                                      {'O', 'B', 'D', 'E', 'F' } ,
+                                      {'G', 'I', 'K', 'L', 'M' } ,
+                                      {'P', 'Q', 'S', 'T', 'U' } ,
+                                      {'V', 'W', 'X', 'Y', 'Z' }
+                                     };
+            const int BLOCKSIZE = 5;
+
+            string rowMatrix = "";
+            string columnMatrix = "";
+
+            int wordCount = 0;
+            int rowCount;
+            int columnCount;
+
+            Console.WriteLine(EncryptionKey.GetLength(0));
+            //find the letter in the matrix and store it's co-ordinates
+            for (rowCount = 0; rowCount <= EncryptionKey.GetLength(0); rowCount++)
+            {
+                for(columnCount = 0; columnCount <= EncryptionKey.GetLength(1); columnCount++)
+                {
+                    if (Convert.ToChar(wordToEncrypt[wordCount]) == EncryptionKey[rowCount,columnCount])
+                    {
+                        rowMatrix = rowMatrix + rowCount;
+                        columnMatrix = columnMatrix + columnCount;
+                        break;
+                    }
+                }
+
+
+            }
+
+
+            return encryptedString
+        }
+
+       
     }
     }
 
